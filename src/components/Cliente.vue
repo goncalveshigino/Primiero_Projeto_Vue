c56c809840c9a3e32a53fbf046e91f8c2c1e8857
goncalveshigino <goncalveshigono20@gmail.com>
2021-03-07 11:58:16 +0100

    first commit

---
+++ src/components/Cliente.vue
@@ -0,0 +1,33 @@
+<template>
+   <div id="cliente">
+    <h2 id=cliente-nome>{{nome}}</h2>
+    <h3> Descricao do cliente: {{descricao}}</h3>
+    {{2*6+5}}
+    <hr>
+   </div>
+</template>
+
+<script>
+
+export default {
+   data(){
+       return {
+           nome:"Gonza Luiz",
+           numero:" ",
+           emai:"",
+           idade:0,
+           descricao:"Filho do tio Higino e da tia Nanda, gajo mau"
+       }
+   }
+}
+</script>
+
+<style scoped>
+
+    #cliente {
+        max-width: 600px;
+        height: 120px;
+    }
+
+    
+</style>
---
+++ src/components/Produto.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+         <h3> Meu Produto</h3>
+         <p>Bla bla bla</p>
+         <input type="text" placeholder="Preço">
+    </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
